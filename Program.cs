5414052b842a007695d9ff903ebe44436e26adc7
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-05 22:41:08 -0300

    falta terminar

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+ 
+namespace GeradorDeJogosLoteria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+ Console.WriteLine();
+            Console.WriteLine("Bem-vindo ao gerador de jogos da loteria!");
+ Console.WriteLine();
+            Console.Write("Quantos jogos você deseja gerar (até 10)? ");
+            int quantidadeDeJogos = int.Parse(Console.ReadLine()!);
+ 
+            if (quantidadeDeJogos > 10)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Desculpe, só é possível gerar até 10 jogos.");
+                Console.WriteLine();
+                return;
+                
+            }
+ 
+            for (int i = 1; i <= quantidadeDeJogos; i++)
+            {
+                Console.Write($"\nJogo {i}: ");
+                for (int j = 1; j <= 6; j++)
+                {
+                    Console.Write($"{random.Next(1, 61)} ");
+                }
+            }
+        }
+    }
+}
